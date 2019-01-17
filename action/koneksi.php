595094cfed60cd71c91dd0f6408d0c38fed8369a
ermaerman <ermafel@gmail.com>
2019-01-17 22:37:49 +0700

    ALHAMDULILLAH SL 15 JUARA HEHEHE

--- action/koneksi.php
+++ action/koneksi.php
@@ -1,12 +1,12 @@
 <?php
 	$db_host = "localhost";
 	$db_user = "root";
-	$db_pass = "sheilaonseven";
+	$db_pass = "";
 	$db_name = "kp";
 
 	$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-	if(mysqli_connect_errno()){
+	if(mysqli_connect_error()){
 		echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
 	}
 ?>
--- action/tambah_nilai.php
+++ action/tambah_nilai.php
@@ -50,6 +50,12 @@
 	$alfa					= $_POST['alfa'];
 	$catatan_guru			= $_POST['catatan_guru'];
 
+	if ($pai > $kkm_pai AND $bindo > $kkm_bindo AND ) {
+		$kelas = $kelas + 1;
+	} elseif {
+		
+	}
+
 	$simpan	= "INSERT INTO input_nilai(id, nis, kelas, semester, pai, pai_huruf, kkm_pai, bindo, bindo_huruf, kkm_bindo, binggris, binggris_huruf, kkm_binggris, barab, barab_huruf, kkm_barab, matematika, matematika_huruf, kkm_matematika, ipa, ipa_huruf, kkm_ipa, ips, ips_huruf, kkm_ips, aqidah, aqidah_huruf, kkm_aqidah, fiqih, fiqih_huruf, kkm_fiqih, qurdis, qurdis_huruf, kkm_qurdis, ski, ski_huruf, kkm_ski, senbud, senbud_huruf, kkm_senbud, penjaskes, penjaskes_huruf, kkm_penjaskes, budi_pekerti, sakit, izin, alfa, catatan_guru) VALUES ('', '$nis', '$kelas', '$semester', '$pai', '$pai_huruf', '$kkm_pai', '$bindo', '$bindo_huruf', '$kkm_bindo', '$binggris', '$binggris_huruf', '$kkm_binggris', '$barab', '$barab_huruf', '$kkm_barab', '$matematika', '$matematika_huruf', '$kkm_matematika', '$ipa', '$ipa_huruf', '$kkm_ipa', '$ips', '$ips_huruf', '$kkm_ips', '$aqidah', '$aqidah_huruf', '$kkm_aqidah', '$fiqih', '$fiqih_huruf', '$kkm_fiqih', '$qurdis', '$qurdis_huruf', '$kkm_qurdis', '$ski', '$ski_huruf', '$kkm_ski', '$senbud', '$senbud_huruf', '$kkm_senbud', '$penjaskes', '$penjaskes_huruf', '$kkm_penjaskes', '$budi_pekerti', '$sakit', '$izin', '$alfa', '$catatan_guru')";
 	
 	$masuk	= mysqli_query($koneksi,$simpan)or die(mysqli_error($koneksi));
